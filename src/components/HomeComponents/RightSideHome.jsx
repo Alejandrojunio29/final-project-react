6903d45ccdaf295fb4efe3aa63bfe7be9aade5c8
alejandro_junio29 <alejandro_junio29@hotmail.com>
2024-01-13 18:38:54 -0600

    format added to the right side

--- src/components/HomeComponents/RightSideHome.jsx
+++ src/components/HomeComponents/RightSideHome.jsx
@@ -1,26 +1,39 @@
+import SmallCardRight from "./SmallCardRight";
+
 export default function RightSideHome() {
   return (
     <>
       <section className=" basis-1/5 hidden lg:flex">
-        <div className=" stats-vertical shadow ">
-          <div className="stat">
-            <div className="stat-title">Downloads</div>
-            <div className="stat-value">31K</div>
-            <div className="stat-desc">Jan 1st - Feb 1st</div>
-          </div>
-
-          <div className="stat">
-            <div className="stat-title">New Users</div>
-            <div className="stat-value">4,200</div>
-            <div className="stat-desc">↗︎ 400 (22%)</div>
-          </div>
-
-          <div className="stat">
-            <div className="stat-title">New Registers</div>
-            <div className="stat-value">1,200</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
-          </div>
-        </div>
+        <ul className=" ">
+          <li className=" list-item border rounded p-2 ">
+            <h1 className=" font-semibold text-lg w-72 hover:text-indigo-500">
+              #discuss
+            </h1>
+            <h5 className="text-xs">
+              Discussion threads targeting the whole community{" "}
+            </h5>
+          </li>
+          <SmallCardRight
+            title="What was your win this week?"
+            comments="6 comments"
+          />
+          <SmallCardRight
+            title="What are you learning about this weekend"
+            comments="3 comments"
+          />
+          <SmallCardRight
+            title="What is your favorite open source alternative to proprietary software"
+            comments="15 comments"
+          />
+          <SmallCardRight
+            title="Mentor Matching Monthly"
+            comments="25 comments"
+          />
+          <SmallCardRight
+            title="2024 Web Development wish list"
+            comments="4 comments"
+          />
+        </ul>
       </section>
     </>
   );
