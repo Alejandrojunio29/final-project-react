dd00b536a66d3385c54222d4468238121b093da1
alejandro_junio29 <alejandro_junio29@hotmail.com>
2024-01-13 23:42:16 -0600

    finish the page PostDetail

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home.jsx";
+import PostDetail from "./pages/PostDetail.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/postDetail", element: <PostDetail /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
