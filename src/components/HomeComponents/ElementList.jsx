c842e1de6b2c8c4c1a285103f366d793c02b9c9d
alejandro_junio29 <alejandro_junio29@hotmail.com>
2024-01-20 00:54:56 -0600

    added some details to the left side menu of home

--- src/components/HomeComponents/ElementList.jsx
+++ src/components/HomeComponents/ElementList.jsx
@@ -1,7 +1,7 @@
 export default function ElementList(props) {
   return (
     <>
-      <li>
+      <li className="hover:bg-indigo-100 hover:underline hover:text-indigo-500">
         <a>
           <img src={props.img} alt="" className="w-7" />
           {props.name}
--- src/components/HomeComponents/LeftSideHome.jsx
+++ src/components/HomeComponents/LeftSideHome.jsx
@@ -1,10 +1,61 @@
 import ElementList from "./ElementList";
+import clsx from "clsx";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import IconLeftSide from "./IconLeftSide";
+
+const TOKEN_KEY2 = "token";
 
 export default function LeftSideHome() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem(TOKEN_KEY2);
+    if (token) {
+      setIsloggedIn(true);
+    }
+  }, []);
+
   return (
     <>
-      <section className=" hidden md:flex  basis-1/8 flex-wrap grow ">
-        <ul className="menu bg-white w-auto rounded-box grow ">
+      <section className=" hidden md:flex flex-col basis-1/8 gap-4 flex-wrap ">
+        <div
+          className={clsx("card px-4 bg-white rounded-md flex w-min", {
+            hidden: isLoggedIn,
+          })}
+        >
+          <h1 className="font-bold text-xl pt-2 flex w-60">
+            DEV Community is a community of 1,241,857 amazing developers
+          </h1>
+          <h3 className="pt-3 pe-3">
+            We're a place where coders share, stay up-to-date and grow their
+            careers.
+          </h3>
+          <div className=" flex flex-col gap-1 pt-3">
+            <button
+              onClick={() => {
+                navigate("/createAccount");
+              }}
+              className={clsx(
+                "btn w-full btn-outline btn-primary hover:underline"
+              )}
+            >
+              Create Account
+            </button>
+            <button
+              onClick={() => {
+                navigate("/login");
+              }}
+              className={clsx(
+                "btn  btn-outline  border-none hover:bg-indigo-200 hover:text-indigo-600 hover:underline"
+              )}
+            >
+              Log in
+            </button>
+          </div>
+        </div>
+        <ul className="menu   rounded-box  ">
           <ElementList
             img="https://img.icons8.com/emoji/48/house-emoji.png"
             name="Home"
@@ -49,6 +100,27 @@ export default function LeftSideHome() {
             img="https://img.icons8.com/emoji/48/thinking-face.png"
             name="Software comparisons"
           />
+          <h2 className="font-bold text-base ps-4 pt-4">Other</h2>
+          <ElementList
+            img="https://img.icons8.com/parakeet/48/thumb-up.png"
+            name="Code of conduct"
+          />
+          <ElementList
+            img="https://img.icons8.com/fluency/48/nerd.png"
+            name="Privacy Policy"
+          />
+          <ElementList
+            img="https://img.icons8.com/emoji/48/eyes-emoji.png"
+            name="Terms of use"
+          />
+          <div className="flex flex-row justify-around pt-4">
+            <IconLeftSide iconLink="https://img.icons8.com/ios-glyphs/30/twitter--v1.png" />
+            <IconLeftSide iconLink="https://img.icons8.com/ios-filled/50/facebook--v1.png" />
+            <IconLeftSide iconLink="https://img.icons8.com/ios-filled/50/github.png" />
+            <IconLeftSide iconLink="https://img.icons8.com/ios-filled/50/instagram-new--v1.png" />
+            <IconLeftSide iconLink="https://img.icons8.com/ios-filled/50/twitch.png" />
+            <IconLeftSide iconLink="https://img.icons8.com/windows/64/mastodon.png" />
+          </div>
         </ul>
       </section>
     </>
